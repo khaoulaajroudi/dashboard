10b2e4a0f8a0f4ecae79f11140bfd7a06fb558a3
khaoulaajroudi <khaoula.a.88@gmail.com>
2023-06-14 11:34:24 +0100

    first

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <div className='dash-header'>
+        <img src='https://img.freepik.com/vecteurs-premium/avatar-du-profil-utilisateur-feminin-est-femme-personnage-pour-economiseur-ecran-emotions_505620-617.jpg?w=740' alt='avatar'></img>
+        <ion-icon name="notifications-outline"></ion-icon>
+        <ion-icon name="chatbox-outline"></ion-icon>
+      </div>
+     <div className='content-box'>   
+<div className='box-left'>
+  <ul>
+    <li><ion-icon name="people-outline"></ion-icon> Users List</li>
+    <li><ion-icon name="list-outline"></ion-icon> Products List</li>
+  </ul>
+</div>
+<div className='right-box'>
+
+</div>
+     </div>
+     </div>
   );
 }
 
